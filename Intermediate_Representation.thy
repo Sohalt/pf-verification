5c9c12a47e0974fa533529dcb966c13ec553bcd9
Paolo Holinski <paolo.holinski@tum.de>
2018-07-25 00:18:17 +0200

    rename Port to Ports for intermediate representation

--- Intermediate_Representation.thy
+++ Intermediate_Representation.thy
@@ -13,8 +13,8 @@ begin
 datatype 'i intermediate_primitive =
 Src "'i wordinterval"
 | Dst "'i wordinterval"
-| Src_Port "16 wordinterval"
-| Dst_Port "16 wordinterval"
+| Src_Ports "16 wordinterval"
+| Dst_Ports "16 wordinterval"
 | IIface "string"
 | OIface "string"
 | Protocol primitive_protocol
@@ -24,8 +24,8 @@ end
 fun itermediate_matcher :: "'i::len intermediate_primitive \<Rightarrow> 'i simple_packet \<Rightarrow> ternaryvalue" where
 "itermediate_matcher (Src a) p = bool_to_ternary (wordinterval_element (p_src p) a)" |
 "itermediate_matcher (Dst a) p = bool_to_ternary (wordinterval_element (p_dst p) a)" |
-"itermediate_matcher (Src_Port port) p = bool_to_ternary (wordinterval_element (p_sport p) port)" |
-"itermediate_matcher (Dst_Port port) p = bool_to_ternary (wordinterval_element (p_dport p) port)" |
+"itermediate_matcher (Src_Ports port) p = bool_to_ternary (wordinterval_element (p_sport p) port)" |
+"itermediate_matcher (Dst_Ports port) p = bool_to_ternary (wordinterval_element (p_dport p) port)" |
 "itermediate_matcher (IIface i) p = bool_to_ternary ((p_iiface p) = i)" |
 "itermediate_matcher (OIface i) p = bool_to_ternary ((p_oiface p) = i)" |
 "itermediate_matcher (Protocol proto) p = bool_to_ternary ((p_proto p) = proto)" |
