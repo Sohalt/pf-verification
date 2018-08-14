59265b029539185d70f92cee72fc8308fea8da0c
Paolo Holinski <paolo.holinski@tum.de>
2018-08-14 12:33:35 +0200

    example transform

---
+++ SemanticsTernary/PF_Example.thy
@@ -0,0 +1,12 @@
+theory PF_Example
+  imports 
+Example_Data
+pfprefix_PF_To_Iptables
+begin
+
+definition ipt_ruleset :: "32 Common_Primitive_Syntax.common_primitive rule list" where
+"ipt_ruleset = pf_to_ipt_v4_upper pf_context pf_ruleset"
+
+value "ipt_ruleset"
+
+end
