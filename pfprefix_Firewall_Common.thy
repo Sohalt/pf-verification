fb3c742acb47cc4ec401309d95abe877fcadf6ee
Paolo Holinski <paolo.holinski@tum.de>
2018-08-14 13:38:57 +0200

    formatting

--- pfprefix_Firewall_Common.thy
+++ pfprefix_Firewall_Common.thy
@@ -38,7 +38,8 @@ abbreviation no_quick :: "'a ruleset \<Rightarrow> bool" where
 "no_quick ls \<equiv> (\<forall> l \<in> set ls. \<not>is_quick_rule l)"
 
 abbreviation no_match :: "'a ruleset \<Rightarrow> bool" where
-"no_match ls \<equiv> (\<forall> l \<in> set ls. (case l of (PfRule r) \<Rightarrow> (pf_rule.get_action r) \<noteq> ActionMatch | _ \<Rightarrow> True))"
+"no_match ls \<equiv> (\<forall> l \<in> set ls. (case l of (PfRule r) \<Rightarrow> (pf_rule.get_action r) \<noteq> ActionMatch
+                                          | _ \<Rightarrow> True))"
 
 definition simple_ruleset :: "'a ruleset \<Rightarrow> bool" where
 "simple_ruleset rs \<equiv> (no_anchors rs \<and> no_quick rs \<and> no_match rs)"
@@ -65,15 +66,17 @@ fun unwrap_decision :: "decision_wrap \<Rightarrow> decision" where
 case_of_simps unwrap_decision_cases: unwrap_decision.simps
 
 
-text\<open>optimizing match expressions\<close>
+text\<open>optimizing match expressions\<close> (* adapted from Iptables_Semantics.Firewall_Common *)
 
 fun optimize_matches_option :: "('a match_expr \<Rightarrow> 'a match_expr option) \<Rightarrow> 'a ruleset \<Rightarrow> 'a ruleset" where
   "optimize_matches_option _ [] = []" |
-  "optimize_matches_option f (PfRule r#rs) = (case f (pf_rule.get_match r) of
-                                               None \<Rightarrow> optimize_matches_option f rs 
-                                               | Some m \<Rightarrow> (PfRule (r\<lparr>pf_rule.get_match := m\<rparr>))#optimize_matches_option f rs)"
+  "optimize_matches_option f (PfRule r#rs) =
+(case f (pf_rule.get_match r) of
+  None \<Rightarrow> optimize_matches_option f rs 
+  | Some m \<Rightarrow> (PfRule (r\<lparr>pf_rule.get_match := m\<rparr>))#optimize_matches_option f rs)"
 
-lemma optimize_matches_option_simple_ruleset: "simple_ruleset rs \<Longrightarrow> simple_ruleset (optimize_matches_option f rs)"
+lemma optimize_matches_option_simple_ruleset:
+"simple_ruleset rs \<Longrightarrow> simple_ruleset (optimize_matches_option f rs)"
   proof(induction rs rule:optimize_matches_option.induct)
   qed(simp_all add: simple_ruleset_def split: option.split)
 
@@ -101,20 +104,18 @@ next
 qed
 
 
-(*
-lemma optimize_matches_option_preserves':
-  "\<forall> m \<in> set rs. P (get_match m) \<Longrightarrow> \<forall>m. P m \<longrightarrow> (\<forall>m'. f m = Some m' \<longrightarrow> P m') \<Longrightarrow> \<forall>m \<in> set (optimize_matches_option f rs). P (get_match m)"
-  using optimize_matches_option_preserves[simplified] by metis
-*)
 lemma optimize_matches_option_append: 
   assumes "simple_ruleset rs1"
       and "simple_ruleset rs2"
-    shows "optimize_matches_option f (rs1@rs2) = optimize_matches_option f rs1 @ optimize_matches_option f rs2"
-  using assms by(induction rs1 rule: optimize_matches_option.induct;auto simp add: simple_ruleset_def split: option.split)
+    shows "optimize_matches_option f (rs1@rs2) =
+           optimize_matches_option f rs1 @ optimize_matches_option f rs2"
+  using assms by (induction rs1 rule: optimize_matches_option.induct)
+                 (auto simp: simple_ruleset_def split: option.split)
 
 
 definition optimize_matches :: "('a match_expr \<Rightarrow> 'a match_expr) \<Rightarrow> 'a ruleset \<Rightarrow> 'a ruleset" where
-  "optimize_matches f rs =  optimize_matches_option (\<lambda>m. (if matcheq_matchNone (f m) then None else Some (f m))) rs"
+  "optimize_matches f rs =
+    optimize_matches_option (\<lambda>m. (if matcheq_matchNone (f m) then None else Some (f m))) rs"
 
 lemma optimize_matches_empty[simp]:
 "optimize_matches f [] = []"
@@ -142,9 +143,11 @@ lemma optimize_matches_simple_ruleset: "simple_ruleset rs \<Longrightarrow> simp
 
 fun optimize_matches_a :: "(action \<Rightarrow> 'a match_expr \<Rightarrow> 'a match_expr) \<Rightarrow> 'a ruleset \<Rightarrow> 'a ruleset" where
 "optimize_matches_a _ [] = []" |
-"optimize_matches_a f (PfRule r#rs) = (PfRule (r\<lparr>pf_rule.get_match := f (pf_rule.get_action r) (pf_rule.get_match r)\<rparr>))#optimize_matches_a f rs"
+"optimize_matches_a f (PfRule r#rs) = 
+(PfRule (r\<lparr>pf_rule.get_match := f (pf_rule.get_action r) (pf_rule.get_match r)\<rparr>))#optimize_matches_a f rs"
 
-lemma optimize_matches_a_simple_ruleset: "simple_ruleset rs \<Longrightarrow> simple_ruleset (optimize_matches_a f rs)"
+lemma optimize_matches_a_simple_ruleset:
+"simple_ruleset rs \<Longrightarrow> simple_ruleset (optimize_matches_a f rs)"
 proof(induction rs)
   case Nil
   then show ?case by simp
@@ -162,7 +165,8 @@ next
 qed
 
 lemma optimize_matches_a_simple_ruleset_eq:
-  "simple_ruleset rs \<Longrightarrow> (\<And> m a. a = Pass \<or> a = Block \<Longrightarrow> f1 a m = f2 a m) \<Longrightarrow> optimize_matches_a f1 rs = optimize_matches_a f2 rs"
+  "simple_ruleset rs \<Longrightarrow> (\<And> m a. a = Pass \<or> a = Block \<Longrightarrow> f1 a m = f2 a m)
+    \<Longrightarrow> optimize_matches_a f1 rs = optimize_matches_a f2 rs"
 proof(induction rs)
   case Nil
   then show ?case by simp
@@ -181,7 +185,8 @@ qed
 lemma optimize_matches_a_preserves:
   assumes "simple_ruleset rs"
   shows "(\<And> r. (PfRule r) \<in> set rs \<Longrightarrow> P (f (pf_rule.get_action r) (pf_rule.get_match r)))
-    \<Longrightarrow> \<forall> r \<in> set (optimize_matches_a f rs). (case r of (PfRule r) \<Rightarrow> P (pf_rule.get_match r) | _ \<Rightarrow> True)"
+    \<Longrightarrow> \<forall> r \<in> set (optimize_matches_a f rs). (case r of (PfRule r) \<Rightarrow> P (pf_rule.get_match r) 
+                                                        | _ \<Rightarrow> True)"
   using assms
 proof(induction rs)
   case Nil
